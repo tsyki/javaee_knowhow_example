e81d1694b82b53e59f8340a96099261cee101c91
Toshiyuki Imaizumi <evening_glow@khf.biglobe.ne.jp>
2016-12-23 23:32:58 +0900

    Selenideでのテスト用画面を追加(PrimeFacesの日付部品とコンボ)

---
+++ src/main/java/tsyki/javaee/example/primefaces/PrimeFacesWidgetView.java
@@ -0,0 +1,70 @@
+package tsyki.javaee.example.primefaces;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+
+/**
+ * PrimeFacesの部品のサンプル
+ * @author TOSHIYUKI.IMAIZUMI
+ * @since 2016/12/23
+ */
+@Named
+@RequestScoped
+public class PrimeFacesWidgetView {
+
+	@NotNull
+	private Date date;
+
+	@NotNull
+	private String comboValue1;
+
+	@NotNull
+	private String comboValue2;
+
+	private String result;
+
+	public void submit(){
+		DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		String dateString =  format.format(date);
+		result = dateString + " " + comboValue1 + " " + comboValue2;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getComboValue1() {
+		return comboValue1;
+	}
+
+	public void setComboValue1(String comboValue1) {
+		this.comboValue1 = comboValue1;
+	}
+
+	public String getComboValue2() {
+		return comboValue2;
+	}
+
+	public void setComboValue2(String comboValue2) {
+		this.comboValue2 = comboValue2;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+
+}
