cf0882173ec0d733a98147c402ee3aced8cbf888
Toshiyuki Imaizumi <evening_glow@khf.biglobe.ne.jp>
2016-12-24 09:24:54 +0900

    Selenideのテストコード追加(コンボの選択)

--- src/test/java/tsyki/javaee/example/primefaces/PrimeFacesWidgetViewBrowserTest.java
+++ src/test/java/tsyki/javaee/example/primefaces/PrimeFacesWidgetViewBrowserTest.java
@@ -2,9 +2,16 @@ package tsyki.javaee.example.primefaces;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.Assert.*;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import com.codeborne.selenide.SelenideElement;
 
 
 /**
@@ -18,10 +25,45 @@ public class PrimeFacesWidgetViewBrowserTest {
 	public void 日付とコンボの入力(){
 		//NOTE -Dselenide.baseUrl=http://localhost:8080/javaee_knowhow_example/faces/ の想定
 		open("primefaces/primeFacesWidgetView.xhtml");
-		$(By.id("widgetSampleForm:date_input")).setValue("2016/01/01");
-		// TODO コンボの選択
+		setDateValue("widgetSampleForm:date", "2016/01/01");
+		setComboValue("widgetSampleForm:combo1", "value1-2");
+		setComboValue("widgetSampleForm:combo2", "value2-2");
 		$("button[type=submit]").click();
 		// アサート
-		$(By.id("widgetSampleForm:result")).shouldHave(text("2016/01/01 value1-1 value2-1"));
+		$(By.id("widgetSampleForm:result")).shouldHave(text("2016/01/01 value1-2 value2-2"));
+	}
+
+	private void setDateValue(String dateId, String date){
+		// NOTE 日付部品はidに_inputが付いたものが実際のinput要素
+		$(By.id(dateId + "_input")).setValue(date);
+		// DatePickerを消しておく
+		$(By.className("ui-datepicker-current-day")).click();
+	}
+
+	private void setComboValue(String comboId,String value){
+		SelenideElement comboRoot = $(By.id(comboId));
+		// まずドロップダウンリストを出す
+		comboRoot.click();
+		SelenideElement selectNode = comboRoot.$("select");
+		List<WebElement> options = new Select(selectNode).getOptions();
+		// NOTE PrimeFacesのComboはselectByValueで選択できない。コンボを押すと別の要素がポップアップしてくる
+		// ポップアップしてくる方の要素はvalueを持っていないため、まずselect要素の中の何番目を選ぶべきかを取得する
+		int selectIndex = -1;
+		for (int i = 0; i < options.size(); i++) {
+			WebElement option = options.get(i);
+			if(value.equals(option.getAttribute("value"))){
+				selectIndex = i + 1;
+				break;
+			}
+		}
+		if(selectIndex == -1){
+			fail("missing value in " + comboId + " value=" + value);
+			return;
+		}
+		// 出てきたドロップダウンリストのルート
+		SelenideElement dropdownElem = $(By.id(comboId + "_items"));
+		SelenideElement targetItemElem= dropdownElem.$("li:nth-of-type(" + selectIndex + ")");
+		targetItemElem.click();
+
 	}
 }
